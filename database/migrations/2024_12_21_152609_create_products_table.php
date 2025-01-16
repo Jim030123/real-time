c22eff8ce107eab5282fe67955f4713a3258e524
Jim030123 <jimngyinggeemicky@gmail.com>
2025-01-16 21:39:18 +0800

    commit

--- database/migrations/2024_12_21_152609_create_products_table.php
+++ database/migrations/2024_12_21_152609_create_products_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product', function (Blueprint $table) {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('productName');
